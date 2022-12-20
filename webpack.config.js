cc418df346e7d15663b67dbd2adbba1bd39dd479
vlad <vladislav.brovikov@gmail.com>
2022-12-20 18:40:05 -0500

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,17 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         resolve: {
           extensions: [".ts", ".tsx", ".js", ".json"],
         },
-        use: "ts-loader",
+        use: ["ts-loader"],
       },
       {
         test: /\.css$/,
