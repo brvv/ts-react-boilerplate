4b4072ac7b48d394312225459c9d63982c850831
vlad <vladislav.brovikov@gmail.com>
2022-12-20 20:19:20 -0500

    add prettier and eslint

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-const container = document.getElementById("app-root")!;
+const container = document.getElementById("app-root") as HTMLElement;
 const root = createRoot(container);
 root.render(<h1>Hello React!</h1>);
